a7f3c8f3926b066b34cf9627c6f697b89fc4d10e
dheerajvatti <dheerajvatti@hotmail.com>
2016-10-02 20:37:36 -0400

    Enter core Create Cluster App

--- src/CreateCluster_Core/Controllers/HomeController.cs
+++ src/CreateCluster_Core/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CreateCluster_Core._code;
+using Microsoft.Rest;
 
 namespace CreateCluster_Core.Controllers
 {
@@ -10,6 +12,37 @@ namespace CreateCluster_Core.Controllers
     {
         public IActionResult Index()
         {
+            var groupName = "ExampleResourceGroup";
+            var storageName = "clustercreatetemplate";
+            var location = "East US";
+            var subscriptionId = "e4337d44-53e5-48eb-b1ba-6652656b470e";
+            var deploymentName = "createlinuxclustertestdhee";
+
+            DeployClass deploycall = new DeployClass();
+
+           // var token =  deploycall.GetAccessTokenAsync();
+            //var credential = new TokenCredentials(token.Result.AccessToken);
+
+
+            //var rgResult = CreateResourceGroupAsync(credential, groupName, subscriptionId, location);
+            //Console.WriteLine(rgResult.Result.Properties.ProvisioningState);
+            ////Console.ReadLine();
+
+            //var dpResult = CreateTemplateDeploymentAsync(credential, groupName, storageName, deploymentName, subscriptionId);
+            //Console.WriteLine(dpResult.Result.Properties.ProvisioningState);
+            //Console.ReadLine();
+
+
+            // DeleteResourceGroupAsync(credential, groupName, subscriptionId);
+            // Console.ReadLine();
+
+            //var dpResult = DeleteResourceGroupAsync(credential, groupName, storageName, deploymentName, subscriptionId);
+            //Console.WriteLine(dpResult.Result.Properties.ProvisioningState);
+            //Console.ReadLine();
+
+
+
+
             return View();
         }
 
---
+++ src/CreateCluster_Core/_code/DeployClass.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateCluster_Core._code
+{
+    public class DeployClass
+    {
+        public  async Task<AuthenticationResult> GetAccessTokenAsync()
+        {
+            var cc = new ClientCredential("055c54b8-5b6f-4bc9-8a59-028d3f70f9b6", "PErJ/G/wvg+UGBUR6W8zQgkq1taS1mGRmVViM5oSXhg=");
+            var context = new AuthenticationContext("https://login.windows.net/15b79de4-baff-453e-8df8-6865beac9b8c");
+            var token = await context.AcquireTokenAsync("https://management.azure.com/", cc);
+            if (token == null)
+            {
+                throw new InvalidOperationException("Could not get the token.");
+            }
+            return token;
+        }
+
+
+        public  async Task<ResourceGroup> CreateResourceGroupAsync(TokenCredentials credential, string groupName, string subscriptionId, string location)
+        {
+            Console.WriteLine("Creating the resource group...");
+            var resourceManagementClient = new ResourceManagementClient(credential)
+            { SubscriptionId = subscriptionId };
+            var resourceGroup = new ResourceGroup { Location = location };
+            return await resourceManagementClient.ResourceGroups.CreateOrUpdateAsync(groupName, resourceGroup);
+        }
+
+        public  async void DeleteResourceGroupAsync(TokenCredentials credential, string groupName, string subscriptionId)
+        {
+            Console.WriteLine("Deleting resource group...");
+            var resourceManagementClient = new ResourceManagementClient(credential)
+            { SubscriptionId = subscriptionId };
+            await resourceManagementClient.ResourceGroups.DeleteAsync(groupName);
+        }
+
+        public  async Task<DeploymentExtended> CreateTemplateDeploymentAsync(TokenCredentials credential, string groupName, string storageName,
+                                                    string deploymentName, string subscriptionId)
+        {
+            Console.WriteLine("Creating the template deployment...");
+            var deployment = new Deployment();
+            deployment.Properties = new DeploymentProperties
+            {
+                Mode = DeploymentMode.Incremental,
+                TemplateLink = new TemplateLink
+                {
+
+                    //https://clustercreatetemplate.blob.core.windows.net/resourcetemplate/template.json
+                    Uri = "https://" + storageName + ".blob.core.windows.net/resourcetemplate/template.json"
+
+                },
+                ParametersLink = new ParametersLink
+                {
+                    //https://clustercreatetemplate.blob.core.windows.net/resourcetemplate/parameters.json
+                    Uri = "https://" + storageName + ".blob.core.windows.net/resourcetemplate/parameters.json"
+                    //".blob.core.windows.net/templates/Parameters.json"
+                }
+            };
+            var resourceManagementClient = new ResourceManagementClient(credential)
+            { SubscriptionId = subscriptionId };
+            return await resourceManagementClient.Deployments.CreateOrUpdateAsync(groupName, deploymentName, deployment);
+        }
+
+    }
+}
